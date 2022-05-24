c9e4982158fc1a1e46add45e88ce187d740a9b1c
Sarath S <sarathsathyan98@gmail.com>
2022-05-24 10:33:40 +0530

    removed comment

--- src/App.js
+++ src/App.js
@@ -6,18 +6,6 @@ import {useState, useEffect} from 'react';
 import PolicyList from './PolicyList';
 
 function App() {
-  const [policy, setPolicy] = useState([])
-
-  useEffect (() => {
-    setPolicy([
-      {
-        policy_id:'123',
-        date_of_purchase: '2018-02-01',
-        customer_id: '451',
-      },
-    ])
-  }, [])
-
   return (
     <div className="App">
       <h3>Insurance Policy List</h3>
